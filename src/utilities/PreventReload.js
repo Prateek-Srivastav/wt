991f62533ca9eacbd4f102905fc342b19398eb9c
prateek-srivastav <prateek.srvastav@gmail.com>
2023-07-05 20:55:42 +0530

    first commit

---
+++ src/utilities/PreventReload.js
@@ -0,0 +1,47 @@
+import { useEffect } from "react";
+import axios from "axios";
+
+import { BaseUrl } from "../BaseUrl";
+import useToken from "../hooks/useToken";
+
+function PreventReload({ children }) {
+  const { token } = useToken();
+
+  useEffect(() => {
+    const handleBeforeUnload = async () => {
+      let user = JSON.parse(localStorage.getItem("user"));
+      let timeRemaining = JSON.parse(localStorage.getItem("time"));
+
+      user.puzzleProgress.timeRemaining = timeRemaining.toString();
+      localStorage.setItem("user", JSON.stringify(user));
+
+      const res = await axios
+        .put(
+          `${BaseUrl}/progress/update`,
+          {
+            puzzleProgress: user.puzzleProgress,
+          },
+          {
+            headers: {
+              Authorization: "Bearer " + token,
+            },
+          }
+        )
+        .catch((err) => {
+          return alert(err.response.data.message);
+        });
+
+      console.log("ABC");
+    };
+
+    window.addEventListener("beforeunload", handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, []);
+
+  return children;
+}
+
+export default PreventReload;
